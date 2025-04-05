4d19ef22aa3833fd0c99190eea190f5b79d02f58
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2025-04-05 14:13:06 +0200

    feat: better types

--- app/routes/submissions/index.tsx
+++ app/routes/submissions/index.tsx
@@ -1,5 +1,37 @@
 import {useLoaderData} from "react-router";
 
+type Speaker = {
+    name: string;
+    twitter: string;
+    bio: string;
+};
+
+type Session = {
+    intendedAudience: string;
+    length: string;
+    format: string;
+    language: string;
+    abstract: string;
+    title: string;
+    room: string;
+    startTime: string;
+    endTime: string;
+    video: string;
+    startTimeZulu: string;
+    endTimeZulu: string;
+    id: string;
+    sessionId: string;
+    conferenceId: string;
+    startSlot: string;
+    startSlotZulu: string;
+    speakers: Speaker[];
+};
+
+type Submissions = {
+    sessions: Session[]
+}
+
+
 export async function loader() {
     const response = await fetch("https://sleepingpill.javazone.no/public/allSessions/javazone_2023", {
         headers: {
@@ -7,15 +39,14 @@ export async function loader() {
         }
     })
 
-    const submissions = await response.json()
+    const submissions = await response.json() as Submissions
 
     return Response.json(submissions.sessions)
 }
 
 export default function Index() {
-    const sessions = useLoaderData()
+    const sessions = useLoaderData() as Session[]
     console.log(sessions)
-    // @ts-ignore
 
     return (
         <>
@@ -23,7 +54,7 @@ export default function Index() {
             <ul>
                 {
                     sessions.map(session => <li key={session.id}><Session title={session.title}
-                                                                                      speakers={session.speakers.map(s => s.name).join(", ")}
+                                                                                      speakers={session.speakers.map(s => s.name)}
                                                                                       category={"FAEN"}/></li>)
                 }
             </ul>
@@ -53,7 +84,7 @@ function Session({title, speakers, category}: { title: string, speakers: string[
         background: "pink",
         color: "chocolate"
     }}>
-        <h2 style={{fontWeight: "bold"}}>{title} ({speakers})</h2>
+        <h2 style={{fontWeight: "bold"}}>{title} ({speakers.join(", ")})</h2>
         <p>{category}</p>
         <p>Stage: Initial</p>
     </div>
